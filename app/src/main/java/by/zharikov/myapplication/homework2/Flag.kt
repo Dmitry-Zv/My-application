9b01f02b78dc5287d4589d4edb100ef6904ecdba
dimaz <zharikovdima3@gmail.com>
2020-09-24 18:48:11 +0300

    Add layout flags and create  listener on buttonFlag

---
+++ app/src/main/java/by/zharikov/myapplication/homework2/Flag.kt
@@ -0,0 +1,12 @@
+package by.zharikov.myapplication.homework2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.zharikov.myapplication.R
+
+class Flag : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flags)
+    }
+}
