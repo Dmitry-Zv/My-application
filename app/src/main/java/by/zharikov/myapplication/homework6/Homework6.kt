0874c56ba44634235f72a61279739d3ea814b648
dimaz <zharikovdima3@gmail.com>
2020-10-15 23:32:59 +0300

    Add new activity and two fragments

--- app/src/main/java/by/zharikov/myapplication/homework6/Homework6.kt
+++ app/src/main/java/by/zharikov/myapplication/homework6/Homework6.kt
@@ -2,11 +2,14 @@ package by.zharikov.myapplication.homework6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import by.zharikov.myapplication.R
 
 class Homework6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homework6)
+
     }
 }
--- app/src/main/java/by/zharikov/myapplication/homework6/PostcardFragment1.kt
+++ app/src/main/java/by/zharikov/myapplication/homework6/PostcardFragment1.kt
@@ -1,10 +1,14 @@
 package by.zharikov.myapplication.homework6
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.navigation.fragment.findNavController
 import by.zharikov.myapplication.R
 import kotlinx.android.synthetic.main.postcard_fragment1.*
@@ -15,14 +19,13 @@ class PostcardFragment1 : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.postcard_fragment1, container,  false)
+        return inflater.inflate(R.layout.postcard_fragment1, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         open_postcard_button.setOnClickListener {
-            val nav = findNavController()
-            nav.navigate(R.id.showPostcardFragment2)
+            findNavController().navigate(R.id.showPostcardFragment2)
         }
     }
 }
